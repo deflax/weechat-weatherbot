62918d1bd4fafba04021d1c4d1a062ba674d3383
Daniel afx <daniel@deflax.net>
2016-06-12 23:40:08 +0300

    import json

--- weatherbot.py
+++ weatherbot.py
@@ -20,8 +20,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import weechat
-import ast
+import weechat, json, time
 
 SCRIPT_NAME = "weatherbot"
 VERSION = "0.8"
